b362f683e4e3d251be7e4b0ae10d615eb9e810f0
NBlancs <bnoel.omar@gmail.com>
2026-02-07 13:22:27 +0800

    added Login.js

--- resq/src/login/Login.js
+++ resq/src/login/Login.js
@@ -12,23 +12,54 @@ const LoginPage = () => {
 	  // login logic dri 
   }
 
-    return (
-     <main classname="login-container">
-	<section className = "login-card">
-         <header>
-           <h1>Welcome Back</h1>
-           <p>Please enter your details to login</p>
-	 </header>
-    )
 
 
+  return  (
+   <main className="login-container">
+      <section className= "login-card">
+        <header>
+          <h1>Welcome Back</h1>
+          <p>Enter your details to sign in</p>
+        </header>
+
+        <form onSubmit={handleSubmit} className="login-form">
+          <div className = "input-group">
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id = "email"
+              value={email}
+              onChange={(e) => setEmail (e.target.value)}
+              placeholder= "name@gmaill.com"
+              required
+              />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type = "password"
+              id = "password"
+              value={password}
+              onChange={(e) => setPassword (e.target.value)}
+              placeholder="********"
+              required
+            />
+          </div>
+
+          <button type="submit" className="login-button"> 
+            Sign In
+          </button>
+        </form>
 
 
+        {/* In case if we add a sign up feature but since only the LGU will be using this I don't think we need to add a sign up page */}
+        {/* <footer>
+          <p>Don't Have an Account? <a href="/signup"></a>Sign Up</p>
+        </footer> */}
+      </section>
+   </main>
 
 
-
-
-
-
-
-
+  );
+};
